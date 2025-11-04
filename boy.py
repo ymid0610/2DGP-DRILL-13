9ac5f5be2e7f5769ba7942cef04d30a14041d8db
ymid0610 <ymid0610@gmail.com>
2025-11-04 23:17:14 +0900

    날아가는 공 추가로 생성될때 zombie:ball 충돌 group 생성

--- boy.py
+++ boy.py
@@ -165,6 +165,7 @@ class Boy:
             game_world.add_object(ball, 1)
             game_world.add_collision_pair('grass:ball', None, ball)
             game_world.add_collision_pair('boy:ball', None, ball)
+            game_world.add_collision_pair('zombie:ball', None, ball)
 
     def get_bb(self):
         return self.x - 20, self.y - 50, self.x + 20, self.y + 40
